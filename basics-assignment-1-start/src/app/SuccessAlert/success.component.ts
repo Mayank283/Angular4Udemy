5d064a19cebce675a9c6a7ec2ed3e3fb9cb6490b
Mayank <mayank@Mayanks-MacBook-Air.local>
2017-11-19 14:15:06 +0530

    First assignment

---
+++ basics-assignment-1-start/src/app/SuccessAlert/success.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-success',
+    template: `<div class="container">
+                 Successfully completed!
+               </div>`,
+    styles: [` div { color: green; }`]
+})
+export class SuccessComponent {
+
+}
